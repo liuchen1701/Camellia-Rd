e7648719dfbf20fa6a61b61781db18c81c2b2ab8
Asianu <l6leung@ucsd.edu>
2018-03-11 17:46:09 -0700

    fixing some navbar style

--- js/activate_about.js
+++ js/activate_about.js
@@ -2,12 +2,16 @@ function about() {
     var about = document.getElementById("about");
 
     var about_height = about.offsetTop;
+	
+		var navbar = document.getElementsByClassName("navbar")[0];
+	
+		var nav_height = navbar.clientHeight;
 
     var current_height = $(window).scrollTop();
 
-    console.log(about_height + " " + current_height);
+    console.log("about@:\t" + about_height + "\nnav@:\t" + nav_height +"\npage@:\t" + current_height);
 
-    if(about_height < current_height + 100) {
+    if(about_height <= current_height + nav_height) {
         var about_button = document.getElementsByClassName("button-to-about")[0];
         about_button.classList.add("live-page");
     } else {
