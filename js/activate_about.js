a6705b00febcce5553042afeb62cff41dc5effdb
Asianu <l6leung@ucsd.edu>
2018-03-15 15:38:21 -0700

    about bold fixed

--- js/activate_about.js
+++ js/activate_about.js
@@ -1,17 +1,18 @@
 function about() {
-    var about = document.getElementById("about");
+		var landing = document.getElementById("landing-about");
+		var landing_height = landing.clientHeight;
 
-    var about_height = about.offsetTop;
+//    var about = document.getElementById("about");
+//    var about_height = about.offsetTop;
 	
 		var navbar = document.getElementsByClassName("navbar")[0];
-	
 		var nav_height = navbar.clientHeight;
 
     var current_height = $(window).scrollTop();
 
-    console.log("about@:\t" + about_height + "\nnav@:\t" + nav_height +"\npage@:\t" + current_height);
+    console.log("landing@:\t" + landing_height + "\nnav@:\t" + nav_height + "\npage@:\t" + current_height);
 
-    if(about_height <= current_height + nav_height) {
+    if(current_height >= landing_height - nav_height) {
         var about_button = document.getElementsByClassName("button-to-about")[0];
         var about_button_r = document.getElementsByClassName("button-to-about-row")[0];
         about_button.classList.add("live-page");
